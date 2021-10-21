96546b2814b07c1ae5a9b65ad05b31c856a2fcfa
geny200 <Geny200k@mail.ru>
2021-10-21 20:07:40 +0300

    Add DataBase

---
+++ HW-3/src/main/java/ru/akirakozov/sd/refactoring/database/DataBase.java
@@ -0,0 +1,19 @@
+package ru.akirakozov.sd.refactoring.database;
+
+import ru.akirakozov.sd.refactoring.domain.Product;
+
+import java.util.List;
+
+public interface DataBase {
+    public void insertIntoProduct(Product product);
+
+    public List<Product> selectAllFromProduct();
+
+    public int countProducts();
+
+    public long sumProducts();
+
+    public Product maxInProducts();
+
+    public Product minInProducts();
+}
