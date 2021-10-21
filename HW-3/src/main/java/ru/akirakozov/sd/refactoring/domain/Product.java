87a502d94908609da86bcc02b4954ff1726ce49f
geny200 <Geny200k@mail.ru>
2021-10-21 20:06:46 +0300

    Add Product domain

---
+++ HW-3/src/main/java/ru/akirakozov/sd/refactoring/domain/Product.java
@@ -0,0 +1,27 @@
+package ru.akirakozov.sd.refactoring.domain;
+
+public class Product {
+    private String name;
+    private long price;
+
+    public Product(String name, long price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
