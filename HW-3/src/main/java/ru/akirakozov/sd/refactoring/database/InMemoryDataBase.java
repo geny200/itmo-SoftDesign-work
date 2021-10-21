81547cedf01f94898aca1e91294ef17a4d5b4d5a
geny200 <Geny200k@mail.ru>
2021-10-21 20:10:04 +0300

    Add InMemory and Dev implementation of DataBase

---
+++ HW-3/src/main/java/ru/akirakozov/sd/refactoring/database/InMemoryDataBase.java
@@ -0,0 +1,52 @@
+package ru.akirakozov.sd.refactoring.database;
+
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
+import ru.akirakozov.sd.refactoring.domain.Product;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class InMemoryDataBase implements DataBase {
+    private final Multiset<Product> dataBase = HashMultiset.create();
+
+    @Override
+    public void insertIntoProduct(Product product) {
+        dataBase.add(product);
+    }
+
+    @Override
+    public List<Product> selectAllFromProduct() {
+        return new ArrayList<>(dataBase);
+    }
+
+    @Override
+    public int countProducts() {
+        return dataBase.size();
+    }
+
+    @Override
+    public long sumProducts() {
+        return dataBase
+                .stream()
+                .map(Product::getPrice)
+                .reduce(0L, (Long::sum));
+    }
+
+    @Override
+    public Product maxInProducts() {
+        return dataBase
+                .stream()
+                .max(Comparator.comparingLong(Product::getPrice))
+                .orElse(null);
+    }
+
+    @Override
+    public Product minInProducts() {
+        return dataBase
+                .stream()
+                .min(Comparator.comparingLong(Product::getPrice))
+                .orElse(null);
+    }
+}
